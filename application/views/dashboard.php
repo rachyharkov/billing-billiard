4830c70fc9f24675198fa27d4b7c880e1dba5d2d
muhammad.ramdan <muhammad.ramdan@spesolution.com>
2022-09-27 22:15:36 +0700

    first commit

--- application/views/dashboard.php
+++ application/views/dashboard.php
@@ -55,88 +55,6 @@
 
 <div id="content" class="content">
 	<div class="row">
-		<center>
-			<script type="text/javascript">
-				//fungsi displayTime yang dipanggil di bodyOnLoad dieksekusi tiap 1000ms = 1detik
-				function tampilkanwaktu() {
-					//buat object date berdasarkan waktu saat ini
-					var waktu = new Date();
-					//ambil nilai jam, 
-					//tambahan script + "" supaya variable sh bertipe string sehingga bisa dihitung panjangnya : sh.length
-					var sh = waktu.getHours() + "";
-					//ambil nilai menit
-					var sm = waktu.getMinutes() + "";
-					//ambil nilai detik
-					var ss = waktu.getSeconds() + "";
-					//tampilkan jam:menit:detik dengan menambahkan angka 0 jika angkanya cuma satu digit (0-9)
-					document.getElementById("clock").innerHTML = (sh.length == 1 ? "0" + sh : sh) + ":" + (sm.length == 1 ? "0" + sm : sm) + ":" + (ss.length == 1 ? "0" + ss : ss);
-				}
-			</script>
-
-			<body onload="tampilkanwaktu();setInterval('tampilkanwaktu()', 1000);">
-				<center>
-					<h1>
-						<span id="clock"></span>
-					</h1>
-				</center>
-				<?php
-				$hari = date('l');
-				/*$new = date('l, F d, Y', strtotime($Today));*/
-				if ($hari == "Sunday") {
-					echo "Minggu";
-				} elseif ($hari == "Monday") {
-					echo "Senin";
-				} elseif ($hari == "Tuesday") {
-					echo "Selasa";
-				} elseif ($hari == "Wednesday") {
-					echo "Rabu";
-				} elseif ($hari == "Thursday") {
-					echo ("Kamis");
-				} elseif ($hari == "Friday") {
-					echo "Jum'at";
-				} elseif ($hari == "Saturday") {
-					echo "Sabtu";
-				}
-				?>,
-				<?php
-				$tgl = date('d');
-				echo $tgl;
-				$bulan = date('F');
-				if ($bulan == "January") {
-					echo " Januari ";
-				} elseif ($bulan == "February") {
-					echo " Februari ";
-				} elseif ($bulan == "March") {
-					echo " Maret ";
-				} elseif ($bulan == "April") {
-					echo " April ";
-				} elseif ($bulan == "May") {
-					echo " Mei ";
-				} elseif ($bulan == "June") {
-					echo " Juni ";
-				} elseif ($bulan == "July") {
-					echo " Juli ";
-				} elseif ($bulan == "August") {
-					echo " Agustus ";
-				} elseif ($bulan == "September") {
-					echo " September ";
-				} elseif ($bulan == "October") {
-					echo " Oktober ";
-				} elseif ($bulan == "November") {
-					echo " November ";
-				} elseif ($bulan == "December") {
-					echo " Desember ";
-				}
-				$tahun = date('Y');
-				echo $tahun;
-				?>
-		</center>
-	</div>
-
-	<ol class="breadcrumb pull-right">
-		<li class="active">Dashboard</li>
-	</ol>
-	<div class="row">
 		<div class="col-md-12">
 			<div class="panel panel-inverse">
 				<div class="panel-heading">
@@ -146,7 +64,7 @@
 						<a href="javascript:;" class="btn btn-xs btn-icon btn-circle btn-warning" data-click="panel-collapse"><i class="fa fa-minus"></i></a>
 						<a href="javascript:;" class="btn btn-xs btn-icon btn-circle btn-danger" data-click="panel-remove"><i class="fa fa-times"></i></a>
 					</div>
-					<h4 class="panel-title">Dashboard</h4>
+					<h4 class="panel-title">Billing</h4>
 				</div>
 				<div class="panel-body">
 					<div class="row">
